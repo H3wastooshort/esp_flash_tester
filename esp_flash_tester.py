99efce9861f178b6d9d56e29c3d6c48490026841
H3wastooshort <hacker3000@posteo.org>
2023-07-31 22:53:54 +0200

    ram test (WIP) (fucked)

--- esp_flash_tester.py
+++ esp_flash_tester.py
@@ -1,16 +1,23 @@
 import esptool
-import os.path, sys, tempfile, time, JSON
+import os.path, sys, tempfile, time, json
 
 baud_rate = "115200"
+ram_size = 80 * 1024
 
+if  len(sys.argv) > 4:
+    ram_size = int(sys.argv[4]) * 1024
 if len(sys.argv) > 3:
     baud_rate = sys.argv[3]
 elif len(sys.argv) < 3:
     quit("Use ith parameters [ESP Port] [Flash Size in MB] (baudrate)")
 
+esp_port = sys.argv[1]
+
 flash_size = int(sys.argv[2]) * 1024 * 1024
 print("ESP on port " + sys.argv[1])
 print("Flash size is %dM (%d bytes)" % (int(sys.argv[2]),flash_size))
+if  len(sys.argv) > 4:
+    print("Testing %d bytes of ram" % ram_size)
 
 workdir=tempfile.mkdtemp()
 #taken from https://github.com/espressif/esptool/blob/fa7e37aa4c0ef0221f25c4aff45bcdca36bf146c/flasher_stub/esptool_test_stub.py
@@ -18,7 +25,7 @@ sys.path.append("..")
 
 print("Connecting... Be sure that the ESP is in the bootloader. Maybe tie GPIO0 to GND")
 cmd_con = [
-        '--port', sys.argv[1],
+        '--port', esp_port,
         '--baud', baud_rate,
         '--after', 'no_reset',
         '--connect-attempts', '0',
@@ -26,12 +33,12 @@ cmd_con = [
     ]
 esptool.main(cmd_con)
 
-def match_files(fname_a,fname_b):
+def match_files(fname_a,fname_b,length):
     file_a = open(fname_a,'rb')
     file_b = open(fname_b,'rb')
     start_idx = None
     bad_parts={}
-    for idx in range (0,flash_size-1):
+    for idx in range (0,length-1):
         if file_a.read(1) != file_b.read(1):
             if start_idx == None:
                 start_idx = idx
@@ -40,31 +47,37 @@ def match_files(fname_a,fname_b):
                 bad_parts[start_idx]=idx-start_idx
                 start_idx = None
     if start_idx != None:
-        bad_parts[start_idx]=flash_size-1-start_idx
+        bad_parts[start_idx]=length-1-start_idx
     
     return bad_parts
 
 
-def exec_test(file_name):
+def exec_test(file_name,mem_type):
+    length = flash_size
+    if mem_type == "mem":
+        ram_size
     cmd_up = [
-        '--port', sys.argv[1],
+        '--port', esp_port,
         '--baud', baud_rate,
         '--after', 'no_reset',
 #        '--before', 'no_reset',
-        'write_flash',
+        'write_'+mem_type,
         '0',
         file_name
     ]
     cmd_dn = [
-        '--port', sys.argv[1],
+        '--port', esp_port,
         '--baud',baud_rate,
         '--after', 'no_reset',
 #        '--before', 'no_reset',
-        'read_flash',
+        'read_'+mem_type,
         '0',
-        str(flash_size),
+        str(length),
         file_name+'_dl'
     ]
+    if mem_type == "mem":
+        cmd_up.append('--no-stub')
+        cmd_dn.append('--no-stub')
     print("=== Writing test data ===")
     try:
         esptool.main(cmd_up)
@@ -77,11 +90,11 @@ def exec_test(file_name):
     return match_files(file_name,file_name+'_dl')
     
 
-test_names = {
-    0:"all_zero",
-    1:"all_one",
-    2:"alternating"
-}
+test_names = [
+    "all_zero",
+    "all_one",
+    "alternating"
+]
 
 test_results={}
 
@@ -91,37 +104,45 @@ def sum_bad_parts(bad_parts):
         bp_sum += bad_parts[k]
     return bp_sum
 
-def test_esp(t):
-    test_name = test_names[t]
+def test_esp(test_name,mem_type):
     print("==== Testing with "+test_name+" ====")
     file_name = os.path.join(workdir, test_name)
     test_dat_file = open(file_name,"wb")
-    if t == 0:
+    if test_name == "all_zero":
         for b in range (0,flash_size-1):
             test_dat_file.write(b'\x00')
-    elif t == 1:
+    elif test_name == "all_one":
         for b in range (0,flash_size-1):
             test_dat_file.write(b'\xff')
-    elif t == 2:
+    elif test_name == "alternating":
         for b in range (0,flash_size-1):
             test_dat_file.write(b'\x55')
     test_dat_file.close()
-    bad_parts = exec_test(file_name)
-    print("\n==== Found %d bad parts totaling %d bytes ====\n" % (len(bad_parts.keys()), sum_bad_parts(bad_parts)))
+    bad_parts = exec_test(file_name,mem_type)
+    print("\n==== Found %d bad parts totaling %d bytes (%d) ====\n" % (len(bad_parts.keys()), sum_bad_parts(bad_parts)))
     test_results[test_name]=bad_parts
 
-test_esp(0)
-test_esp(1)
-test_esp(2)
+def test_memory_type(mem_type):
+    if mem_type not in ["flash","mem"]:
+        quit("unknown memory type")
+    
+    print("====== Testing %s ======" % mem_type)
+    
+    for test_name in test_names:
+        test_esp(test_name,mem_type)
 
-print("\n\n")
+    print("\n\n")
 
-res_print = input("Print JSON results? [y/N] ")
-if res_print == "y" or res_print == "Y":
-    print(test_results)
+    res_print = input("Print JSON results? [y/N] ")
+    if res_print == "y" or res_print == "Y":
+        print(test_results)
 
-res_fn = input("Save JSON results as (leave blank to skip): ")
-if len(res_fn)>0:
-    res_f = open(res_fn,'w')
-    res_f.write(JSON.dumps(test_results))
-    res_f.close()
+    res_fn = input("Save JSON results as (leave blank to skip): ")
+    if len(res_fn)>0:
+        res_f = open(res_fn,'w')
+        res_f.write(json.dumps(test_results))
+        res_f.close()
+
+if  len(sys.argv) > 4:
+    test_memory_type("mem")
+test_memory_type("flash")
